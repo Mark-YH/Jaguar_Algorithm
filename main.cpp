3ad832e361e1b4f0dbc2ca31904cb29466152c9f
Mark Hsu <s108321505@mail1.ncnu.edu.tw>
2019-12-10 15:28:56 +0800

    Added while-loop for multiple experiments

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@
 
 int main() {
     std::remove("../log/ja.epin");
-    std::remove("../log/hunting1.csv");
+    std::remove("../log/hunting.csv");
     std::remove("../log/see_around.csv");
     std::remove("../log/speed_down.csv");
     std::remove("../log/speed_up.csv");
     auto start = std::chrono::steady_clock::now();
     srand(114);
-    JA ja;
-    ja.run();
+    int k = 0;
+    while (k < 1) {
+        JA ja;
+        ja.run();
+        k++;
+    }
     auto end = std::chrono::steady_clock::now();
     std::cout << "Time taken: " << std::chrono::duration<double>(end - start).count() << "s" << std::endl;
     return 0;
